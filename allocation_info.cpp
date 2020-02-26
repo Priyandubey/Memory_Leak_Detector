9027f5dd77413816aa28649468cdd48cc560e0c2
priyandubey <priyanshudubey74@gmail.com>
2020-02-26 22:25:10 +0530

    implemented .cpp and .hpp allocation_info

---
+++ allocation_info.cpp
@@ -0,0 +1,32 @@
+#include "allocation_info.hpp"
+
+namespace leakdetector{
+
+    namespace memory{
+
+        allocation_info::allocation_info(void* address,size_t size,char** stacktrace,size_t depth,pthread_t thread_id){
+            this->address = address;
+            this->size = size;
+            this->thread_id = thread_id;
+            for(int i = 1;i < depth;i++){
+                std::string frame = stacktrace[i];
+                this->stacktrace.push_back(frame);
+            }
+        }
+
+        allocation_info::address_type allocation_info::get_address(){
+            return address;
+        }
+        size_t allocation_info::get_size(){
+            return size;
+        }
+        vector<std::string> allocation_info::get_stacktrace(){
+            return stacktrace;
+        }
+        pthread_t allocation_info::get_thread_id(){
+            return thread_id;
+        }
+
+    }
+
+}
